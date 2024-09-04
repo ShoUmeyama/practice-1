ff4fc82764b1c00640e1cb99899820f2d936ec96
ShoUmeuyama <sho.ume.2004@gmail.com>
2024-09-04 17:47:53 +0900

    add/ヘッター＆ページタイトル

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from './Header.module.css'
+
+export default function Home() {
+    return (
+        <header className={styles.header}>
+            <h2 className={styles.h2}>これがヘッターです</h2>
+            <nav className={styles.nav}>
+                <Link href="/">index</Link>
+                <Link href="/about">about</Link>
+            </nav>
+        </header>
+    );
+}
