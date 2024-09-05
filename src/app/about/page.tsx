5eda8417b679a5b38c8457f9267bca99b72f4934
ShoUmeuyama <sho.ume.2004@gmail.com>
2024-09-05 21:38:40 +0900

    add ImageExplain

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,16 @@
+import React from 'react';
+import BodyText from '../components/ImageExplain';
+
+
 export default function about() {
     return (
-        <h1>こんばんは</h1>
+        <div>
+            <h1>こんばんは</h1>
+            <BodyText 
+                title="皆さんこんにちは私の名前は田中富勝です。27歳なんですけども、よく80代みたいな名前だねと言われます。
+                        皆さんこんにちは私の名前は田中富勝です。27歳なんですけども、よく80代みたいな名前だねと言われます。"
+                imageUrl="/images/pig.jpg"
+                imageText='写真が読み込まれていません'/>
+        </div>
     );
 }
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 export default function Home() {
     return (
         <header className={styles.header}>
-            <h2 className={styles.h2}>これがヘッターです</h2>
+            <h2 className={styles.headerTitle}>これがヘッターです</h2>
             <nav className={styles.nav}>
                 <Link href="/">index</Link>
                 <Link href="/about">about</Link>
